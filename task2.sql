88da6fc6965ce82104c23b0319911b0763cf88d3
sivaprasadgandepalli <84697701+sivaprasadgandepalli@users.noreply.github.com>
2023-03-08 00:43:32 +0530

    Update task2.sql

--- task2.sql
+++ task2.sql
@@ -1,4 +1,4 @@
--- select*from workertab where salary between 100000 and 500000;
--- select *from workertab where char_length(first_name)=6 and first_name like"h%";
--- select * from workertab where join_date like '2014-02-20%';
--- select *from workertab where salary>=50000 and salary<=100000;
+Q9:select*from workertab where salary between 100000 and 500000;
+Q10:select *from workertab where char_length(first_name)=6 and first_name like"h%";
+Q11:select * from workertab where join_date like '2014-02-20%';
+Q12:select *from workertab where salary>=50000 and salary<=100000;
